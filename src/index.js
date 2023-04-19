f865ba4822b768351ce96f0e07ca7741a06bf8cf
shilat hakimi <shilat57@gmail.com>
2023-04-19 13:47:23 +0300

    fir

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+  console.log('A new request has arrived to index.js');
+  res.send('Hello main Page 3');
+});
+
+app.listen(port, () => {
+  console.log(`Server is up and runnig at port: ${port}`);
+});
---
+++ tests/Arrays.spec.js
@@ -0,0 +1,13 @@
+import { expect } from "chai";
+
+describe('Arrays', ()=>{
+
+    describe('#sort', ()=>{
+
+        it('Sorting names array', ()=>{
+            var names = ['Dani', 'Moshe', 'Adam'];
+            expect(names.sort()).to.be.eql(['Adam', 'Dani', 'Moshe']);
+        })
+
+    })
+})
