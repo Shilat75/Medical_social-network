1e6947693249eddd330b95b9f7c21f55b9e89239
shilat hakimi <shilat57@gmail.com>
2023-04-20 13:17:57 +0300

    http

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ const app = express();
 
 app.get('/', (req, res) => {
   console.log('A new request has arrived to index.js');
-  res.send('file:///C:/Users/Administrator/Project_manag/pages/Homepage.html');
+ // res.send(file://C:/Users/Administrator/Project_manag/pages/Homepage.html');
+  res.sendFile( /Users/Administrator/Project_manag+ "/pages/Homepage.html");
 });
 
 app.listen(port, () => {
