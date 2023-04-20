8a72a48b92a69abe23b8a19973900be8bf740db6
reut <r9822501@gmail.com>
2023-04-20 14:20:51 +0300

    try again

--- src/index.js
+++ src/index.js
@@ -3,20 +3,13 @@ import express from 'express';
 const port = process.env.PORT || 3000;
 const app = express();
 
-//app.get('/', (req, res) => {
- // console.log('A new request has arrived to index.js');
- // res.send(file://C:/Users/Administrator/Project_manag/pages/Homepage.html');
-  //res.sendFile( /Users/Administrator/Project_manag+ "/pages/Homepage.html");
-//g});
-
 app.get('/', (req, res) => {
+  console.log('A new request has arrived to index.js');
   res.sendFile('pages/Homepage.html');
 });
 
-// app.get('/Login', (req, res) => {
-//   res.sendFile('pages/login.html', { root: './' });
-// });
-
+ 
+ 
 app.listen(port, () => {
   console.log(`Server is up and runnig at port: ${port}`);
 });
