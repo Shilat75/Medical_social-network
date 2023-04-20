baca7508b8928b577138a92fcd8a9de9974b2f31
shilat hakimi <shilat57@gmail.com>
2023-04-20 13:29:23 +0300

    sercf

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const app = express();
 //app.get('/', (req, res) => {
  // console.log('A new request has arrived to index.js');
  // res.send(file://C:/Users/Administrator/Project_manag/pages/Homepage.html');
-  res.sendFile( /Users/Administrator/Project_manag+ "/pages/Homepage.html");
-//});
+  //res.sendFile( /Users/Administrator/Project_manag+ "/pages/Homepage.html");
+//g});
 
 app.get('/', (req, res) => {
   res.sendFile('pages/index.html', { root: './' });
