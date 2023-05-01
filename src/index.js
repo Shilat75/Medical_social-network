8cd41b74ff8b77e54030ff12694dd1cb4f6cbd7f
shilat hakimi <shilat57@gmail.com>
2023-05-01 16:13:47 +0300

    Port in login 5432

--- src/index.js
+++ src/index.js
@@ -1,41 +1,37 @@
 import express from 'express';
-import pkg from 'pg';
-const {Client} = pkg;
 
-const client = new Client({
- user: 'shilat',
- host: 'dpg-ch0llaj3cv2c5b5o7nug-a',
- database: 'd_nbwf',
- password: '0J3XBf05m6IfzTX8YHH46OFruMkj2mPM',
- port: 5432,
- ssl: true
+import pkg from 'pg';
+
+const { Client } = pkg;
+
+Client({
+  user: 'shilat',
+  host: 'dpg-ch0llaj3cv2c5b5o7nug-a',
+  database: 'd_nbwf',
+  password: '0J3XBf05m6IfzTX8YHH46OFruMkj2mPM',
+  port: 5432,
+  ssl: true,
 });
-client.connect();
 const port = process.env.PORT || 80;
 const app = express();
 app.use(express.static('pages'));
 
+
 app.use(express.json());
 
-
 app.get('/', (req, res) => {
-   res.sendFile('pages/Homepage.html', { root: './' });
+  res.sendFile('pages/Homepage.html', { root: './' });
 });
-app.get('/HomePage.css', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile('pages/HomePage.css', { root: './' });
 });
 app.get('/Login', (req, res) => {
   res.sendFile('pages/Login.html', { root: './' });
 });
-<<<<<<< HEAD
-app.get('/Login.css', (req, res) => {
-=======
 app.get('/', (req, res) => {
->>>>>>> 798018034cb7ec04b722140c8aedb5e09df7701b
   res.sendFile('pages/Login.css', { root: './' });
 });
 
-
 app.listen(port, () => {
-   console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
