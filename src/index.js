47a78f34aba04680a7f58fdc0ba836f62ac6c701
tairmazuz20 <tairma@ac.sce.ac.il>
2023-05-25 16:19:22 +0300

    try to fix

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ const express = require('express');
 
 const mongoose = require('mongoose');
 const path = require('path');
-const authRouter = require('../Controllers/auth');
-
+// const authRouter = require('../Controllers/auth');
 
 const port = process.env.PORT || 80;
 const app = express();
@@ -11,7 +10,7 @@ const app = express();
 const dbURI = 'mongodb+srv://tairmazuz19:0532217639@nosecl.evkn28f.mongodb.net/';
 // const dbURI = 'mongodb://0.0.0.0:27017';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => {
+  .then((_result) => {
     console.log('Connected to MongoDB');
     app.listen(port, () => {
       console.log(`Listening on port ${port}`);
@@ -27,28 +26,28 @@ db.once('open', () => {
 });
 app.use(express.static('pages'));
 app.use(express.json());
-//app.use('/auth', authRouter);
+// app.use('/auth', authRouter);
 
-app.get('/', (req, res) => {
+app.get('/', (_req, _res) => {
   rxes.sendFile(path.join(__dirname, '../pages/Homepage.html'));
 });
-app.get('/HomePage.css', (req, res) => {
+app.get('/HomePage.css', (_req, res) => {
   res.sendFile(path.join(__dirname, '../pages/HomePage.css'));
 });
-app.get('/Login', (req, res) => {
+app.get('/Login', (_req, res) => {
   res.sendFile(path.join(__dirname, '../pages/Login.html'));
 });
-app.get('/Login.css', (req, res) => {
+app.get('/Login.css', (_req, res) => {
   res.sendFile(path.join(__dirname, '../pages/Login.css'));
 });
-app.get('/register', (req, res) => {
+app.get('/register', (_req, res) => {
   res.sendFile(path.join(__dirname, '../pages/register.html'));
 });
-app.get('/register.css', (req, res) => {
+app.get('/register.css', (_req, res) => {
   res.sendFile(path.join(__dirname, '../pages/register.css'));
 });
 // Add POST route for '/register'
-app.post('/register', (req, res) => {
+app.post('/register', (_req, res) => {
   // Handle registration logic here
   res.send('Registration successful');
 });
