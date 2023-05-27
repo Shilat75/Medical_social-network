549e7ef6744d5766ff12cf457e82ad3609c702a3
Gitit Dadon <gititdadon@gmail.com>
2023-05-27 22:40:24 +0200

    try againnnn

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const authRouter = require('./../Controllers/auth');
-const User = require('../models/user');
+const authController = require('./Controllers/auth');
+const User = require('./models/user');
 
 const port = process.env.PORT || 80;
 const app = express();
-// render work
+
 // Connect to MongoDB
 const dbURI = 'mongodb+srv://tairmazuz19:0532217639@nosecl.evkn28f.mongodb.net/';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => {
+  .then(() => {
     console.log('Connected to MongoDB');
     app.listen(port, () => {
       console.log(`Listening on port ${port}`);
@@ -26,39 +26,38 @@ db.once('open', () => {
   console.log('Database connection established!');
 });
 
+// Middleware
 app.use(express.static('pages'));
 app.use(express.json());
-//app.use('/auth', authRouter);
 
-/*app.get('/auth', (req, res) => {
-  res.sendFile(path.join(__dirname, authRouter));
-});*/
-app.post('/register', (req, res) => {
-  // Handle registration logic here
-  // You can call your signup function from the authController here
-  authController.signup(req, res);
-});
-
+// Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../pages/Homepage.html'));
+  res.sendFile(path.join(__dirname, 'pages', 'Homepage.html'));
 });
+
 app.get('/HomePage.css', (req, res) => {
-  res.sendFile(path.join(__dirname, '../pages/HomePage.css'));
+  res.sendFile(path.join(__dirname, 'pages', 'HomePage.css'));
 });
+
 app.get('/Login', (req, res) => {
-  res.sendFile(path.join(__dirname, '../pages/Login.html'));
+  res.sendFile(path.join(__dirname, 'pages', 'Login.html'));
 });
+
 app.get('/Login.css', (req, res) => {
-  res.sendFile(path.join(__dirname, '../pages/Login.css'));
+  res.sendFile(path.join(__dirname, 'pages', 'Login.css'));
 });
+
 app.get('/register', (req, res) => {
-  res.sendFile(path.join(__dirname, '../pages/register.html'));
+  res.sendFile(path.join(__dirname, 'pages', 'register.html'));
 });
+
 app.get('/register.css', (req, res) => {
-  res.sendFile(path.join(__dirname, '../pages/register.css'));
+  res.sendFile(path.join(__dirname, 'pages', 'register.css'));
 });
-// Add POST route for '/register'
-app.post('/register', (req, res) => {
-  // Handle registration logic here
-  res.send('Registration successful');
+
+app.post('/register', authController.signup);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
