b3df543390a6e54bd135c0cc5497cfc008a0f0fc
Gitit Dadon <gititdadon@gmail.com>
2023-05-29 09:36:50 +0200

    rr

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,56 +1,41 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const User=require('../models/user');
+const authController = require('../Controllers/auth');
+const User = require('../models/user');
 
-const validateEmail = (email) => {
-    // Regular expression to validate email format
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  };
-
-
-
-
-
-
+//router.post('/signup', authController.signup);
 router.post('/register', async (req, res) => {
-    const { email, username, password, confirmpassword } = req.body;
+    const { email, username, password } = req.body;
     const level = 'starter';
     console.log(email);
     console.log(username);
     console.log(password);
-    console.log(confirmpassword);
     try {
       const existingUser = await User.findOne({ email });
       if (existingUser) {
         return res.render('register', { error: 'The email provided is already registered. Please try with a different email.' });
       }
-      if (username.length < 4 && username.length > 20) {
-        return res.render('register', { error: 'Username must have at least 4 characters' });
-      }
+      
       if (!validateEmail(email)) {
         return res.render('register', { error: 'Invalid email format' });
       }
       if (password.length < 6 && password.length > 20) {
         return res.render('register', { error: 'Password must be between 6 and 20 characters long' });
       }
-      if (password !== confirmpassword) {
-        return res.render('register', { error: 'Passwords do not match' });
-      }
+      
       const hashedPassword = await bcrypt.hash(password, 10);
       const user = new User({ email, password: hashedPassword, username, level });
       await user.save();
-      res.redirect('/login');
+      res.redirect('/Login');
     } catch (err) {
       console.error(err);
       res.redirect('/register');
     }
   });
-
-router.get('/logout', (req, res) => {
-  req.logout();
-  res.redirect('/login');
-});
+  router.get('/register', (req, res) => {
+    const message = req.flash('message')[0];
+    res.render('register', { error: '' });
+  });
+router.post('/signin', authController.signin);
 
 module.exports = router;
