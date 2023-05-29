ff8ebb12b902c26aecf450cc58355950d008bd92
Gitit Dadon <gititdadon@gmail.com>
2023-05-29 08:26:20 +0200

    changed registers

--- pages/register.js
+++ pages/register.js
@@ -1,100 +1,217 @@
-const form = document.getElementById('login-form');
-const usernameInput = document.getElementById('username-input');
-const emailInput = document.getElementById('email-input');
-const passwordInput = document.getElementById('password-input');
-const nameInput = document.getElementById('name-input');
-const addressInput = document.getElementById('address-input');
-const phoneInput = document.getElementById('phone-input');
-const submitBtn = document.querySelector('button[type="submit"]');
+document.querySelector('.img__btn').addEventListener('click', function () {
+  document.querySelector('.cont').classList.toggle('s--signup');
+});
+// Get DOM elements
+const signInForm = document.querySelector('.form.sign-in');
+const signUpForm = document.querySelector('.form.sign-up');
+const signUpButton = document.querySelector('.img__btn .m--up');
+const signInButton = document.querySelector('.img__btn .m--in');
+const emailInputs = document.querySelectorAll('input[type="email"]');
+const passwordInputs = document.querySelectorAll('input[type="password"]');
+const userNameInput = document.querySelector('input[type="text"]');
+const signInSubmitButton = signInForm.querySelector('.submit');
+const signUpSubmitButton = signUpForm.querySelector('.submit');
 
 
-    // Function to validate email format
-    function validateEmail(email) {
-      const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      return emailPattern.test(email);
-    }
-  
-    // Function to validate password length
-    function validatePassword(password) {
-        const minLength = 8;
-        const hasSpecialCharacter = /[\s~`!@#$%^&*+=\-[\]\\';,/{}|\\":<>?()._]/.test(password);
-        
-        return password.length >= minLength && hasSpecialCharacter;
-      }
-  
-   
-  
-    // Function to hide error message
-    function hideError(element) {
-      const errorContainer = element.nextElementSibling;
-      errorContainer.textContent = ' ';
-    }
-  
-    // Function to show error message
-    function showError(element, message) {
-      const errorContainer = element.nextElementSibling;
-      errorContainer.textContent = message;
-    }
-  
-    // Function to handle form submission
-    function validateForm() {
-      //event.preventDefault();
-      hideError(emailInput);
-      hideError(passwordInput);
-      hideError(usernameInput);
-  
-      const email = emailInput.value.trim();
-      const password = passwordInput.value.trim();
-      const username = usernameInput.value.trim();
-  
-      let hasError = false;
-  
-      if (!validateEmail(email)) {
-        showError(emailInput, 'Invalid email, try again.');
-        hasError = true;
-      }
-  
-      if (!validatePassword(password)) {
-        showError(passwordInput, 'Password must be between 6 and 20 characters.');
-        hasError = true;
-      }
-  
-      if (!validateUsername(username)) {
-        showError(usernameInput, 'Username must be between 4 and 20 characters.');
-        hasError = true;
-      }
-  
-      if (hasError) {
-        return;
-      }
-  
-      // Perform sign up logic here
-      fetch('/auth/signup', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email: email,
-          password: password,
-          username: username,
-        }),
-      })
-        .then(response => {
-          if (response.ok) {
-            console.log('Sign up successful');
-            signInForm.reset();
-          } else {
-            throw new Error('Sign up failed');
-          }
-        })
-        .catch(error => {
-          console.error('Registration error:', error);
-          console.error(error.stack);
-  
-          // Display the error message to the user
-          showError(emailInput, 'The user already exists.');
-        });
-    }
-  
-  
+
+// Add event listeners for switching between sign-in and sign-up forms
+signInButton.addEventListener('click', () => {
+  signUpForm.style.display = 'none';
+  signInForm.style.display = 'block';
+});
+
+signUpButton.addEventListener('click', () => {
+  signInForm.style.display = 'none';
+  signUpForm.style.display = 'block';
+});
+
+// Add event listeners for form submissions
+signInSubmitButton.addEventListener('click', signIn);
+signUpSubmitButton.addEventListener('click', signUp);
+
+// Email validation regex pattern
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+// Function to validate email format
+function validateEmail(email) {
+  return emailPattern.test(email);
+}
+
+// Function to validate password length
+function validatePassword(password) {
+  const minLength = 6;
+  const maxLength = 20;
+  return password.length >= minLength && password.length <= maxLength;
+}
+
+// Function to validate username length
+function validateUserName(username) {
+  const minLength = 4;
+  const maxLength = 20;
+  return username.length >= minLength && username.length <= maxLength;
+}
+
+
+// Function to hide error message
+function hideError(element) {
+  const errorContainer = element.parentElement.querySelector('.error-message');
+  errorContainer.textContent = '';
+  element.classList.remove('error');
+}
+
+// Function to handle sign in form submission
+// Function to handle sign in form submission
+
+function signIn(event) {
+  event.preventDefault();
+  console.log('Sign in');
+  const errorMsgIn = document.getElementById('error-msg-in');
+  errorMsgIn.textContent = '';
+  // Validate email and password
+  const email = signInForm.querySelector('input[type="email"]').value;
+  const password = signInForm.querySelector('input[type="password"]').value;
+
+  if (!validateEmail(email)) {
+    errorMsgIn.textContent = 'Invalid email, try again. ';
+    return;
+  }
+
+  if (!validatePassword(password)) {
+    errorMsgIn.textContent = 'Password must be between 6 and 20 characters';
+    return;
+  }
+
+  // Perform sign in logic here
+  fetch('/auth/signin', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+  })
+  .then(response => response.json())
+  .then(data => window.location.href = data.redirect)
+  .catch(error => {
+      console.error('Sign in error:', error);
+      console.error(error.stack);
+
+      // Display the error message to the user
+      const errorMsgUp = document.getElementById('error-msg-in');
+      errorMsgUp.textContent = 'Login failed, verify your details again..';
+    });
+}
+
+
+
+// function signIn(event) {
+//   event.preventDefault();
+//   console.log('Sign in');
+//   const errorMsgIn = document.getElementById('error-msg-in');
+//   errorMsgIn.textContent = '';
+//   // Validate email and password
+//   const email = signInForm.querySelector('input[type="email"]').value;
+//   const password = signInForm.querySelector('input[type="password"]').value;
+
+//   if (!validateEmail(email)) {
+//     errorMsgIn.textContent = 'Invalid email, try again. ';
+//     return;
+//   }
+
+//   if (!validatePassword(password)) {
+//     errorMsgIn.textContent = 'Password must be between 6 and 20 characters';
+//     return;
+//   }
+
+//   // Perform sign in logic here
+//   fetch('/auth/signin', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({
+//       email: email,
+//       password: password,
+//     }),
+//   })
+//   .then(response => response.json())
+//   .then(data => window.location.href = data.redirect)
+//   .catch(error => {
+//       console.error('Sign in error:', error);
+//       console.error(error.stack);
+
+//       // Display the error message to the user
+//       const errorMsgUp = document.getElementById('error-msg-in');
+//       errorMsgUp.textContent = 'Login failed, verify your details again..';
+//     });
+// }
+// Function to handle sign up form submission
+function signUp(event) {
+  event.preventDefault();
+  console.log('Sign up');
+  const errorMsgUp = document.getElementById('error-msg-up');
+  errorMsgUp.textContent = '';
+  // Validate email, password, and username
+  const email = signUpForm.querySelector('input[type="email"]').value;
+  const password = signUpForm.querySelector('input[type="password"]').value;
+
+  // Retrieve the value of the "confirm-password" field
+  const confirmPassword = signUpForm.querySelector('input[type="password"][name="confirm-password"]').value;
+
+  // Retrieve the value of the username input field
+  const username = userNameInput.value.trim();;
+  console.log(username);
+  if (username === '') {
+    errorMsgUp.textContent = 'Enter a username';
+    return;
+  }
+  if (!validateEmail(email)) {
+    errorMsgUp.textContent = 'Invalid email, try again. ';
+    return;
+  }
+
+  if (!validatePassword(password)) {
+    errorMsgUp.textContent = 'Password must be between 6 and 20 characters';
+    return;
+  }
+
+  if (password !== confirmPassword) {
+    errorMsgUp.textContent = 'Passwords do not match';
+
+    return;
+  }
+
+  // Perform sign up logic here
+  fetch('/auth/signup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+      username: username,
+    }),
+  })
+    .then(response => response.json())
+    .then(data => window.location.href = data.redirect)
+    .catch(error => {
+      console.error('Registration error:', error);
+      console.error(error.stack);
+
+      // Display the error message to the user
+      const errorMsgUp = document.getElementById('error-msg-up');
+      errorMsgUp.textContent = 'The user already exists.';
+    });
+
+}
+
+function showError(element, message) {
+  const errorContainer = element.parentElement.querySelector('.error-message');
+  if (errorContainer) {
+    errorContainer.textContent = message;
+    element.classList.add('error');
+  }
+}
