1ccdd2f250c4465828f74f8f6233e4fbfe58f126
Gitit Dadon <gititdadon@gmail.com>
2023-05-29 12:58:59 +0200

    gffffffffff

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -11,37 +11,37 @@ exports.signup = (req, res) => {
     name: 'about my new blog',
   });
 
-  //user.save()
+  user.save()
 
-//   const { email, password, username } = req.body;
-//   const level = 'starter';
-//   // Check if the user already exists in the database
-//   User.findOne({ $or: [{ email }, { username }] })
-//     .then(existingUser => {
-//       if (existingUser) {
-//         if (existingUser.email === email) {
-//           res.status(400).json({ error: 'Email already exists' });
+  const { email, password, username } = req.body;
+  const level = 'starter';
+  // Check if the user already exists in the database
+  User.findOne({ $or: [{ email }, { username }] })
+    .then(existingUser => {
+      if (existingUser) {
+        if (existingUser.email === email) {
+          res.status(400).json({ error: 'Email already exists' });
 
-//         }
-//       } else {
-//         // Create a new user
-//         const newUser = new User({ email, password, username ,level});
-//        //try
+        }
+      } else {
+        // Create a new user
+        const newUser = new User({ email, password, username ,level});
+       //try
 
-//         //end of try
-//         // Save the user to the database
-//         newUser.save()
-//           .then(() => {
-//             res.status(200).json({ message: 'Sign up successful' });
-//           })
-//           .catch(error => {
-//             console.error('Sign up error:', error);
-//             res.status(500).json({ error: 'Internal server error' });
-//           });
-//       }
-//     })
-//     .catch(error => {
-//       console.error('User lookup error:', error);
-//       res.status(500).json({ error: 'Internal server error' });
-//     });
-// };
+        //end of try
+        // Save the user to the database
+        newUser.save()
+          .then(() => {
+            res.status(200).json({ message: 'Sign up successful' });
+          })
+          .catch(error => {
+            console.error('Sign up error:', error);
+            res.status(500).json({ error: 'Internal server error' });
+          });
+      }
+    })
+    .catch(error => {
+      console.error('User lookup error:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    });
+};
