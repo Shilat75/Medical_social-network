df18852e1adbe76cb68687dcfd11ed3f8432997f
reut <r9822501@gmail.com>
2023-05-31 17:54:35 +0300

    post

---
+++ pages/home.js
@@ -0,0 +1,41 @@
+function addPost(event) {
+    event.preventDefault();
+    console.log('add post');
+    const errorMsgIn = document.getElementById('error-msg-in');
+    errorMsgIn.textContent = '';
+    // Validate email and password
+    const email = signInForm.querySelector('input[type="email"]').value;
+    const password = signInForm.querySelector('input[type="password"]').value;
+  
+    if (!validateEmail(email)) {
+      errorMsgIn.textContent = 'Invalid email, try again. ';
+      return;
+    }
+  
+    if (!validatePassword(password)) {
+      errorMsgIn.textContent = 'Password must be 8 characters';
+      return;
+    }
+  
+    // Perform sign in logic here
+    fetch('/auth/signin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    })
+      .then(response => response.json())
+      .then(data => window.location.href = data.redirect)
+      .catch(error => {
+        console.error('Sign in error:', error);
+        console.error(error.stack);
+  
+        // Display the error message to the user
+        errorMsgIn.textContent = 'Login failed, verify your details again..';
+      });
+  }
+  
