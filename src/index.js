88be2384ff29787c4345218b257a2d98f63964bb
Gitit Dadon <gititdadon@gmail.com>
2023-06-01 11:57:10 +0300

    added error msj on login & register

--- src/index.js
+++ src/index.js
@@ -80,22 +80,22 @@ app.post('/register', async (req, res) => {
     res.status(400).json({ success: false, error: err.message });
   }
 });
-// Set up the route for the login page
 app.post('/Login', (req, res) => {
   const { username, password } = req.body;
   // Check if the username and password exist in the database
   User.findOne({ username, password }, (err, user) => {
     if (err) {
       console.error(err);
-      res.status(500).send('User Doesnt Exist In The System!');
+      res.status(500).send('An error occurred');
     } else if (user) {
       // Redirect to the home page
       res.redirect('/home');
     } else {
-      // Show an error message
-      res.send('Invalid username or password');
+      // Show an error message on the browser
+      res.status(401).send({ errorMessage: 'Invalid username or password' });
     }
   });
 });
+
 
 module.exports = app;
