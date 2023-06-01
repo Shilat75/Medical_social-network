94b72f5f178574e32165c73c0df8955c0811e939
Gitit Dadon <gititdadon@gmail.com>
2023-06-01 14:33:41 +0300

    changed index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const { validateEmail, validatePassword } = require('../pages/register');
 const User = require('../models/user');
-
 
 const port = process.env.PORT || 3000;
 const app = express();
-
-
 
 // Connect to MongoDB
 const dbURI = 'mongodb+srv://tairmazuz19:0532217639@nosecl.evkn28f.mongodb.net/';
@@ -25,7 +23,7 @@ db.on('error', (err) => {
   console.log(err);
 });
 db.once('open', () => {
-  console.log('Database connection established!');
+  console.log('Database connection established!!');
 });
 
 app.use(express.static('pages'));
@@ -60,9 +58,7 @@ app.get('/register.css', (req, res) => {
 
 app.get('/personalArea', (req, res) => {
   res.sendFile(path.join(__dirname, '../pages/personalArea.html'));
- 
 });
-
 
 app.get('/personalArea.css', (req, res) => {
   res.sendFile(path.join(__dirname, '../pages/personalArea.css'));
@@ -72,17 +68,36 @@ app.post('/register', async (req, res) => {
   const { email, username, password } = req.body;
 
   try {
-    const newUser = await User.create({
-      email,
-      username,
-      password,
-      level: 'starter',
-    });
+    if (validatePassword(password) && validateEmail(email)) {
+      const newUser = await User.create({
+        email,
+        username,
+        password,
+        level: 'starter',
+      });
 
-    res.status(201).json({ success: true, data: newUser });
+      res.redirect('/Login');
+    }
   } catch (err) {
-    res.status(400).json({ success: false, error: err.message });
+    res.redirect(`/Register?error=${encodeURIComponent(err.message)}`);
+    // res.status(400).json({ success: false, error: err.message });
   }
+});
+app.post('/Login', (req, res) => {
+  const { username, password } = req.body;
+  // Check if the username and password exist in the database
+  User.findOne({ username, password }, (err, user) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('An error occurred');
+    } else if (user) {
+      // Redirect to the home page
+      res.redirect('/home');
+    } else {
+      // Show an error message on the browser
+      res.status(401).send({ errorMessage: 'Invalid username or password' });
+    }
+  });
 });
 
 module.exports = app;
