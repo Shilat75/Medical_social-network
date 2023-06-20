120aab8866e15b0677ed6d77b3bf9b3c8f76da9f
Gitit Dadon <gititdadon@gmail.com>
2023-06-20 18:27:17 +0300

    WORKING UPDATE DETAILS ALERT!!!!!!!!!!!!!!!!!!!!

--- routes/update-user.js
+++ routes/update-user.js
@@ -1,38 +1,38 @@
 var express = require('express');
 const User = require('../models/user');
-const {validateEmail,validatePassword}=require('../tests/logintest.test')
 var router = express.Router();
+const { validateEmail, validatePassword } = require('../tests/logintest.test')
 
 router.post("/", async function (req, res, next) {
-  if (!req.session.user._id) {
-    return res.redirect('login', {
-      layout: true,
-      page: 'login',
-      req
-    });
-  }
-
-  try {
-    const { email, password } = req.body;
-
-    // Validate email and password
-    if (validateEmail(email) && validatePassword(password)) {
-      await User.findByIdAndUpdate(req.session.user._id, req.body);
-      let user = await User.findOne({ _id: req.session.user._id });
-
-      // Update the user details    
-      req.session.user = user;
-
-      res.redirect('/personalArea/view');
-    } else {
-      // Display an error alert on the screen
-      const errorMessage = 'Invalid email or password.';
-      return res.send(`<script>alert('${errorMessage}'); window.location.href='/personalArea/view';</script>`);
+    if (!req.session.user._id) {
+        return res.redirect('login', {
+            layout: true,
+            page: 'login',
+            req
+        });
     }
-  } catch (err) {
-    console.log(err);
-    res.status(400).json({ success: false, error: err.message });
-  }
+
+    try {
+        const { email, password } = req.body;
+
+        // Validate email and password
+        if (validateEmail(email) && !validatePassword(password)) {
+            await User.findByIdAndUpdate(req.session.user._id, req.body);
+            let user = await User.findOne({ _id: req.session.user._id });
+
+            // Update the user details    
+            req.session.user = user;
+
+            res.redirect('/personalArea/view');
+        } else {
+            // Display an error alert on the screen
+            const errorMessage = 'Invalid email or password.';
+            return res.send(`<script>alert('${errorMessage}');</script>`);
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(400).json({ success: false, error: err.message });
+    }
 });
 
 module.exports = router;
