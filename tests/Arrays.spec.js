dfa60f3dc05c5ee1d768da2ff25d8b93ee159d46
tairmazuz20 <tairma@ac.sce.ac.il>
2023-06-20 20:38:03 +0300

    delete array test

--- tests/Arrays.spec.js
+++ tests/Arrays.spec.js
@@ -1,4 +1,4 @@
-
+/*
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../app'); // Replace '../app' with the actual path to your Express app file
@@ -43,3 +43,4 @@ describe('Login', () => {
         });
     });
 });
+*/
